08f18a5f5f8e93d83e8cfad16785a53f79fa211a
Mariano Vazquez <marianodvazquez@gmail.com>
2020-04-20 11:06:19 -0300

    Update packages/exos-scripts/src/scripts/test/jest.config.js

--- packages/exos-scripts/src/scripts/test/jest.config.js
+++ packages/exos-scripts/src/scripts/test/jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  // If the CI environment variable is set, runc overage
+  // If the CI environment variable is set, run coverage
   collectCoverage: !!process.env.CI,
 };
