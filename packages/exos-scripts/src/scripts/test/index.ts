632661ecd39d72a70f9091f2bdf224811e66c2fc
Nano Vazquez <marianodvazquez@gmail.com>
2020-04-20 11:04:23 -0300

    fix(exos-scripts): Remove auto set up of Jest properties in test script

--- packages/exos-scripts/src/scripts/test/index.ts
+++ packages/exos-scripts/src/scripts/test/index.ts
@@ -6,7 +6,6 @@ if (!process.env.NODE_ENV) {
 
 import * as jest from "jest";
 import getConfigToUse from "../../common/getConfigToUse";
-import hasArgument from "../../common/hasArgument";
 import jestConfig = require("./jest.config");
 import type { Config } from "@jest/types";
 
@@ -15,19 +14,6 @@ const configToUse = getConfigToUse<Config.Argv>("test.js", jestConfig as any);
 console.info(configToUse.isCustom ? `Found custom test config at ${configToUse.customConfigPath}` : "Using default test config");
 
 const argv = process.argv.slice(2);
-const isCI = !!process.env.CI;
-
-// If the CI environment variable is set,
-// run coverage (unless explicitly declared otherwise)
-if (isCI && !hasArgument(argv, "--coverage")) {
-  argv.push("--coverage");
-}
-
-// If it's no CI and we arenot collecting coverage,
-// run in watchAll mode (unless explicitly declared otherwise)
-if (!isCI && !hasArgument(argv, "--coverage") && !hasArgument(argv, "--watchAl")) {
-  argv.push("--watchAll");
-}
 
 // Run Jest with config and arguments
 argv.push("--config", JSON.stringify(configToUse.config));
